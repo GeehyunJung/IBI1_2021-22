a70592fbeaa85b26338fb64d248258f7f30aaa45
Geehyun <selina@Geehyun.local>
2022-03-14 21:19:06 +0800

    final

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -3,6 +3,6 @@ a=1
 while n!=64:
 	n=(a**2+a+2)/2
 	a=a+1
-	print(str(n)+' slices of pizza can be cut with '+str(a)+' straight cuts.)
+	print(str(n)+' slices of pizza can be cut with '+str(a)+' straight cuts.')
 	if n>=64:
 		break
