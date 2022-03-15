0becb1961738ce24d48406e75189b66e782cd0bd
Geehyun <selina@Geehyun.local>
2022-03-15 16:39:54 +0800

    e is larger and the rate of new case is greater in 2021.

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,8 +3,12 @@ b=4218520
 c=271
 d=b-c
 e=a-b
-d>=e
+if d>e:
+	print('d is larger and the rate of new cases in 2020 is greater.')
+else:
+	print('e is larger and the rate of new cases in 2021 is greater.')
+#answer:e is larger and the rate of new  cases in 2021 is greater
 X=4>6
 Y=5<7
-X and Y
-X or Y
+W=X  and Y
+print (W)
