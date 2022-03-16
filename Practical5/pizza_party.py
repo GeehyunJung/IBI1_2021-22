666dfeb92d2ce4a143f3417f3ab7932e78eae1a9
Geehyun <selina@Geehyun.local>
2022-03-16 14:53:01 +0800

    final

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,12 +1,13 @@
 # n is the number of slices of the pizza
 # a is the nubmer of straight cuts
-n=1
+n=2
 a=1
+print(str(n)+' slices of pizza can be cut with '+str(a)+' straight cut.')
 # if n does not equals to 64 than increase the nuber of straight cuts
 while n!=64:
+	a=a+1
 	n=(a**2+a+2)/2
 	print(str(n)+' slices of pizza can be cut with '+str(a)+' straight cuts.')
-	a=a+1
 # if n becomes larger than 64, stop cutting
 	if n>=64:
 		break
