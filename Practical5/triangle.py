7b1bb0482883147ec60f06afb778f7745c80073d
Geehyun <selina@Geehyun.local>
2022-03-17 16:45:40 +0800

    more commits added

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -3,7 +3,10 @@
 a=1
 b=1
 print(a)
-#let b plus one each time and add b to a. Then a is the total number of dots. Repeat the process for 9 times.
+# let the number of the dots in the last line plus one each time
+# add the number of the dots in the last line to the total number of dots in the triangle.
+# Then a is the total number of dots.
+# Repeat the process for 9 times.
 for i in range(1,10):
 	b=b+1
 	a+=b
