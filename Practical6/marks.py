d5b6539d752ba6ea2c28be3da71d72d8c763e254
Geehyun <selina@Geehyun.local>
2022-03-22 21:08:23 +0800

    eanline

--- Practical6/marks.py
+++ Practical6/marks.py
@@ -2,6 +2,6 @@ marks=[45,36,86,57,53,92,65,45]
 print(sorted(marks))
 import numpy as np
 import matplotlib.pyplot as plt
-plt.boxplot(marks,vert=True,notch = False)
-show.plt()
+plt.boxplot(marks,vert=True,meanline=True,notch = False)
+plt.show()
 
