589c7dc7c4d22764645ab8701eafd2dd80405630
Geehyun <selina@Geehyun.local>
2022-03-23 14:49:19 +0800

    commits added

--- Practical6/marks.py
+++ Practical6/marks.py
@@ -1,9 +1,15 @@
 marks=[45,36,86,57,53,92,65,45]
 print(sorted(marks))
+#codes from matplot.org about how to create a boxplot
 import numpy as np
 import matplotlib.pyplot as plt
-plt.boxplot(marks,vert=True,showmeans=True,meanline=True,notch = False)
+plt.boxplot(marks,
+vert=True,#vertical or not
+showmeans=True,#show the mean or not
+meanline=True,#show the mean in line or not
+notch = False)
 plt.show()
+#calculate the mean of the marks
 mean=sum(marks)/len(marks)
 if mean>=60:
 	print('Rob passed IBI!!')
