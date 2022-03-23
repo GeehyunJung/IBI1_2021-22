8bcffa755245e382415303a5ec85087065ca6ed6
Geehyun <selina@Geehyun.local>
2022-03-23 21:51:13 +0800

    not sure

--- Practical6/practical6.py
+++ Practical6/practical6.py
@@ -1,7 +1,18 @@
 paternal_age=[30,35,40,45,50,55,60,65,70,75]
 chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
-a={30:1.03,35:1.07,40:1.11,45:1.17,50:1.23,55:1.32,60:1.42,65:1.55,70:1.72,75:1.94}
-print (a)
 import matplotlib.pyplot as plt
-plt.plot((paternal_age), (chd), 'ro')
+import numpy as np
+x=np.array(paternal_age)
+y=np.array(chd)
+
+# fit a linear curve an estimate its y-values and their error.
+a, b = np.polyfit(x, y, deg=1)
+y_est = a * x + b
+y_err = x.std() * np.sqrt(1/len(x) +
+                          (x - x.mean())**2 / np.sum((x - x.mean())**2))
+fig, ax = plt.subplots()
+ax.plot(x, y_est, '-')
+ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
+ax.plot(x, y, 'o', color='tab:brown')
 plt.show()
+
