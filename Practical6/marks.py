3f7e21f1f1ee0a640a7c65124ee8b5419dd01c2c
Geehyun <selina@Geehyun.local>
2022-03-23 14:44:48 +0800

    with python3

--- Practical6/marks.py
+++ Practical6/marks.py
@@ -2,6 +2,11 @@ marks=[45,36,86,57,53,92,65,45]
 print(sorted(marks))
 import numpy as np
 import matplotlib.pyplot as plt
-plt.boxplot(marks,vert=True,notch = False)
+plt.boxplot(marks,vert=True,showmeans=True,meanline=True,notch = False)
 plt.show()
+mean=sum(marks)/len(marks)
+if mean>=60:
+	print('Rob passed IBI!!')
+else:
+	print('Rob failed IBI. (saaaaaad TOT)')
 
