6044ee6bee647ca4ac6e6427d466a19463b62fce
Geehyun <selina@Geehyun.local>
2022-03-28 19:15:16 +0800

    123

---
+++ Practical7/covid.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+os.chdir('/Users/selina/Desktop/')
+covid_data=pd.read_csv('full_data.csv')
+firstandthird=[True,False,True,False,False,False]
+print(covid_data.loc[10:20,firstandthird])
+location_only=[False,True,False,False,True,False]
+print(covid_data.loc[(covid_data['location']=='Afghanistan'),location_only])
