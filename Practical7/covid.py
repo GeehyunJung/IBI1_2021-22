aaff66cf352df19e30fc5b56970e3105e4bc4c9c
Geehyun <selina@Geehyun.local>
2022-03-28 19:22:12 +0800

    123

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-os.chdir('/Users/selina/Desktop/')
+os.chdir('/Users/selina/Desktop/a/1/IBI/IBI1_2021-22/Practical7/')
 covid_data=pd.read_csv('full_data.csv')
 firstandthird=[True,False,True,False,False,False]
 print(covid_data.loc[10:20,firstandthird])
