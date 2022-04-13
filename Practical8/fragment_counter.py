9cb65440808635d54ff015f94ba0a254932edb1f
Geehyun <selina@Geehyun.local>
2022-04-13 11:16:38 +0800

    fragment finished

---
+++ Practical8/fragment_counter.py
@@ -0,0 +1,6 @@
+seq= 'ATGCAATCGACTACGATCAATCGAGGGCC'
+number=seq.find('GAATTC')
+if number>0:
+    print(str(number+1)+' fragments will be cut.')
+else:
+    print('This sequence will not be cut if we applied the EcoRI enzyme to it.')
