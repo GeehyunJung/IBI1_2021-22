32e41edd18347bb9737d9d06c3f7689a80cb543a
Geehyun <selina@Geehyun.local>
2022-04-16 10:24:24 +0800

    finished

--- Practical8/get_cut_genes.py
+++ Practical8/get_cut_genes.py
@@ -6,16 +6,21 @@ myfile=open('cut_genes.fa','w')
 gene_number=0
 gene=[]
 current_gene=''
+# '>' indicates a new line
 for line in xfile:
+    #delete \n between lines
     line=line.replace('\n','')
     if line.startswith('>'):
         gene_number+=1
+        #if 'GAATTC' in line, write it in the cut_genes.fa
         if gene_number>1:
             if 'GAATTC' in current_gene:
                 myfile.write(current_gene+'\n')
+        #if line do not startswith '>' add the line to the current gene to make it in one line
         current_gene=''
         continue
     current_gene+=line
+# add the last line
 if 'GAATTC' in current_gene:
     myfile.write(current_gene)
 myfile.close()
