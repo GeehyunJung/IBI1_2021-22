18467dcbee1463f4ecd2f324ce3b20e3f22243dc
Geehyun <selina@Geehyun.local>
2022-04-18 17:28:00 +0800

    first version

---
+++ Practical10/nucleotide_content_calculator.py
@@ -0,0 +1,16 @@
+# input the sequence by the user
+print('Plesase input your DNA sequence here: ')
+origin_sequence=input()
+# define a function that can change the sequence into upper letters
+def sequence(x):
+    x=list(x.upper())
+    #calculate the percentage of different necleotides
+    A_percentage=x.count('A')/len(x)
+    T_percentage=x.count('T')/len(x)
+    C_percentage=x.count('C')/len(x)
+    G_percentage=x.count('G')/len(x)
+    print(x)
+    print(A_percentage,T_percentage,C_percentage,G_percentage)
+    return x
+# use the function
+new_sequence=sequence(origin_sequence)
