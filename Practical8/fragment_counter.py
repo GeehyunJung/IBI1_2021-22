949dc9c1039243f0663e4fc5200daa567c4c8fcd
Geehyun <selina@Geehyun-2.local>
2022-05-24 21:20:59 +0800

    rechecked,final version

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -3,4 +3,4 @@ number=seq.find('GAATTC')
 if number>0:
     print(str(number+1)+' fragments will be cut.')
 else:
-    print('This sequence will not be cut if we applied the EcoRI enzyme to it.')
+    print('This sequence will not be cut if we applied the EcoRI enzyme to it.\nSo there is only one fragment')
